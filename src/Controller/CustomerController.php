096791c3dddf884dfeb6c871ed2222cd65456f35
Andreas Unger <andreasunger@MacBook-Pro.fritz.box>
2025-07-03 17:34:53 +0200

    Update Account Setting

--- src/Controller/CustomerController.php
+++ src/Controller/CustomerController.php
@@ -2,7 +2,11 @@
 
 namespace App\Controller;
 
+
+use App\Form\ProfileType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
@@ -13,6 +17,22 @@ final class CustomerController extends AbstractController
     {
         return $this->render('customer/index.html.twig', [
             'controller_name' => 'CustomerController',
+        ]);
+    }
+    #[Route('/edit', name: 'profile_edit')]
+    public function edit(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $user = $this->getUser();
+        $form = $this->createForm(ProfileType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->flush();
+            $this->addFlash('success', 'Profil erfolgreich aktualisiert.');
+            return $this->redirectToRoute('profile_edit');
+        }
+        return $this->render('customer/edit.html.twig', [
+            'form' => $form->createView(),
         ]);
     }
 }
